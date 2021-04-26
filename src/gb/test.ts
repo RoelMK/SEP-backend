20980ce7212bcd0bee7b3094d04113bb9e4ee9e2
Rinse <rinse@vlaswinkel.info>
2021-04-26 13:07:59 +0200

    Added basic token handler authentication

--- src/gb/test.ts
+++ src/gb/test.ts
@@ -1,9 +1,12 @@
 // Example file on how to use this setup
 
 import { GameBusClient } from './GameBusClient';
+// Make sure you have a (valid) API token in .env.local
+require('dotenv').config({ path: './env.local' });
+const token = process.env.TEST_TOKEN;
 
 async function main() {
-    const gamebus = new GameBusClient();
+    const gamebus = new GameBusClient(true, token);
     const activity = await gamebus.activity().getActivityById(0);
     console.log(JSON.stringify(activity));
 }

e59d373dfd7fac2b2142251bda9bdc67f7b336eb
Roel Koopman <r.m.koopman@student.tue.nl>
2021-04-29 12:35:55 +0200

    Added JWT

--- src/routes/test.ts
+++ src/routes/test.ts
@@ -1,3 +1,5 @@
+import { checkJwt } from "../middlewares/checkJwt";
+
 const testRouter = require('express').Router();
 
 testRouter.get('/', (req: any, res: any) => {
@@ -6,6 +8,10 @@ testRouter.get('/', (req: any, res: any) => {
 
 testRouter.get('/test', (req: any, res: any) => {
     res.send('Test is working!');
+});
+
+testRouter.get('/jwt-test', checkJwt, (req: any, res: any) => {
+    res.send('Authenticated test, user id: ' + req.user.userId);
 });
 
 module.exports = testRouter;

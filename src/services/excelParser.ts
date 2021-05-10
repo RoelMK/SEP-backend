190339e1d118d8a0a6de43f228e1f0b8c98c2bb0
20181880 <riklitjens@hotmail.nl>
2021-05-10 11:07:41 +0200

    Updated Excel parser: async + array of objects

--- src/services/excelParser.ts
+++ src/services/excelParser.ts
@@ -1,24 +1,32 @@
-import XLSX from 'xlsx';
+import XLSX from "xlsx"
 
 
 /**
  * Default class for parsing .xlsx files
  */
-export default class ExcelParser {
+export default class ExcelParser<D extends {} = Record<string, string>> {
 
-    constructor(){}
+    // resulting data from Excel parse
+    protected rawData: D[] = [];
 
-     parse(filePath: string, skipLine: boolean = false):  unknown[]{ //async Promise<unknown[]>
+    constructor(private readonly filePath: string){}
+
+    async parse(interfaceKeys: string[]):  Promise<D[]>{ // 
             
-            const workbook = XLSX.read(filePath, { type: 'file' });
+            const workbook = XLSX.read(this.filePath, { type: 'file' });
             const [firstSheetName] = workbook.SheetNames;
-            const worksheet = workbook.Sheets[firstSheetName];
-
-             //new Promise((resolve) => {
-           return XLSX.utils.sheet_to_json(worksheet, {
-                raw: true, // Use raw values (true) or formatted strings (false)
-                header: 1, // Generate an array of arrays ("2D Array")
-            });
-        //});
+            const worksheet = workbook.Sheets[firstSheetName];           
+        
+           return new Promise((resolve) => {
+               const resultData: D[] = XLSX.utils.sheet_to_json(worksheet, {
+                raw: false, // Use raw values (true) or formatted strings (false)
+                header: interfaceKeys, // use keys of interface
+                range: 1, // if keys are specified under header property, the package does not remove the header so start at 1
+                defval: '' // standard value for missing values
+                }) as D[];
+                this.rawData = resultData;
+                resolve(resultData);
+        });
     }
 }
+

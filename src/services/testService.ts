f0f9657d008ab4076f245c559fda19a4c08bf8f7
20181880 <riklitjens@hotmail.nl>
2021-05-11 10:23:52 +0200

    Updated test-service for food diaries

--- src/services/testService.ts
+++ src/services/testService.ts
@@ -1,19 +1,23 @@
-import AbbottParser, { AbbottDataType } from './abbottParser';
-import FoodDiaryParser from './foodDiaryParser';
+import AbbottParser from './dataParsers/abbottParser';
+import { OutputDataType } from './dataParsers/dataParser';
+import FoodDiaryParser from './dataParsers/foodDiaryParser';
 
 async function testAbbott() {
     const abbottParser: AbbottParser = new AbbottParser('src/services/glucose/glucose_data_abbott_eu.csv');
     // Currently this step is required since reading the file is async
     await abbottParser.process();
     // Print data for debugging
-    //console.log(abbottParser.getData(AbbottDataType.FOOD));
-    //console.log(abbottParser.getData(AbbottDataType.GLUCOSE));
-    console.log(abbottParser.getData(AbbottDataType.INSULIN));
+    //console.log(abbottParser.getData(OutputDataType.FOOD));
+    //console.log(abbottParser.getData(OutputDataType.GLUCOSE));
+    console.log(abbottParser.getData(OutputDataType.INSULIN));
 }
 
 async function testExcel() {
-    var testPath = 'src/services/food/foodDiary_standard.xlsx'
-    console.log(await new FoodDiaryParser(testPath).process())
+    var testPath = 'src/services/food/foodDiary_standard_missing.xlsx'
+    const foodDiaryParser: FoodDiaryParser = new FoodDiaryParser(testPath, true);
+    await foodDiaryParser.process();
+    console.log(foodDiaryParser.getData(OutputDataType.INSULIN));
+    console.log(foodDiaryParser.getData(OutputDataType.FOOD));
 }
 
 //testAbbott();

c7df3490346e3e23d524798a0074e5747a8cc46b
Rinse <rinse@vlaswinkel.info>
2021-05-13 10:58:24 +0200

    Added working callback URL handling for GameBus connection

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,14 +3,16 @@ import { createJWT, validateConnectRequest } from '../utils/authUtils';
 
 const router = Router();
 
-router.post('/connect', (req: Request, res: Response) => {
-    let userId = req.body.player_id;
-    let accessToken = req.body.access_token;
-    let refreshToken = req.body.refresh_token;
+router.post('/gamebus/callback', (req: Request, res: Response) => {
+    // This is now provided in the request URL as queries
+    let userId = req.query.player_id as string;
+    let accessToken = req.query.access_token as string;
+    let refreshToken = req.query.refresh_token as string;
 
     if (userId && accessToken && refreshToken) {
         if (validateConnectRequest(userId, accessToken)) {
             let jwt: string = createJWT(userId, accessToken, refreshToken);
+            //console.log(jwt);
             return res.status(200).json({ token: jwt }); // TODO: this token must be saved locally on the computer of the end-user.
         } else {
             return res.status(403).send(); // Invalid user id and/or token provided
@@ -19,5 +21,7 @@ router.post('/connect', (req: Request, res: Response) => {
         return res.status(400).send(); // Bad request
     }
 });
+
+// TODO: add DELETE endpoint on Disconnect URL (/gamebus/disconnect)
 
 module.exports = router;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
-require('dotenv').config({ path: __dirname.split('\\').slice(0, -1).join('\\') + '\\.env.local' });
+require('dotenv').config({ path: __dirname.split('\\').slice(0, -1).join('\\') + '\\.env' });
 const express = require('express');
 const errorhandler = require('errorhandler');
+const cors = require('cors');
+import winston from 'winston';
+import expressWinston from 'express-winston';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 // Create app object
 const app = express();
 const port = 8080;
+
+// This is so you can see every incoming request (from GameBus) in console
+app.use(
+    expressWinston.logger({
+        transports: [new winston.transports.Console()],
+        format: winston.format.combine(winston.format.colorize(), winston.format.prettyPrint())
+    })
+);
+
+// TODO: cors might not be needed
+app.use(cors());
 app.use(express.json());
 
 // ---- Do not add routes above this line! ----

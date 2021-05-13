f06f2997df641e61a059db776d7db7890f5ad966
Rinse <rinse@vlaswinkel.info>
2021-05-13 21:34:05 +0200

    Fixed Eetmeter test case with date parsing

--- test/services/food.test.ts
+++ test/services/food.test.ts
@@ -23,46 +23,50 @@ test('import Abbott US food', async () => {
 });
 
 test('import single Eetmeter entry', async () => {
-    let expectedResult = [{
-        timestamp: 1622876400000,
-        calories: 27.92,
-        carbohydrates: 6.98,
-        fat: 4.72,
-        saturatedFat: 0.52,
-        salt: 1.31,
-        sugars: 0.98,
-        water: 46.5,
-        description: 'Vegetarische balletjes'
-    }]
-    
-      var result = await parseEetmeter('test/services/data/eetmeter.xml')
+    let expectedResult = [
+        {
+            timestamp: parse('5/5/2021 9:00', DateFormat.EETMETER, new Date()).getTime(),
+            calories: 27.92,
+            carbohydrates: 6.98,
+            fat: 4.72,
+            saturatedFat: 0.52,
+            salt: 1.31,
+            sugars: 0.98,
+            water: 46.5,
+            description: 'Vegetarische balletjes'
+        }
+    ];
+
+    var result = await parseEetmeter('test/services/data/eetmeter.xml');
     expect(result).toStrictEqual(expectedResult);
 });
 
 test('import many Eetmeter entries', async () => {
-    let expectedResult = [{
-        timestamp: 1622876400000,
-        calories: 27.92,
-        carbohydrates: 6.98,
-        fat: 4.72,
-        saturatedFat: 0.52,
-        salt: 1.31,
-        sugars: 0.98,
-        water: 46.5,
-        description: 'Vegetarische balletjes'
-      },
-      {
-        timestamp: 1622876400000,
-        calories: 46,
-        carbohydrates: 11.5,
-        fat: 1.75,
-        saturatedFat: 0.25,
-        salt: 1.42,
-        sugars: 2,
-        water: 228,
-        description: 'Vegetable soup'
-      }];
-    
-      var result = await parseEetmeter('test/services/data/eetmeterMany.xml')
+    let expectedResult = [
+        {
+            timestamp: parse('5/5/2021 9:00', DateFormat.EETMETER, new Date()).getTime(),
+            calories: 27.92,
+            carbohydrates: 6.98,
+            fat: 4.72,
+            saturatedFat: 0.52,
+            salt: 1.31,
+            sugars: 0.98,
+            water: 46.5,
+            description: 'Vegetarische balletjes'
+        },
+        {
+            timestamp: parse('5/5/2021 9:00', DateFormat.EETMETER, new Date()).getTime(),
+            calories: 46,
+            carbohydrates: 11.5,
+            fat: 1.75,
+            saturatedFat: 0.25,
+            salt: 1.42,
+            sugars: 2,
+            water: 228,
+            description: 'Vegetable soup'
+        }
+    ];
+
+    var result = await parseEetmeter('test/services/data/eetmeterMany.xml');
     expect(result).toStrictEqual(expectedResult);
 });

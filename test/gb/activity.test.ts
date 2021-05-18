28e20cd9559a873c7a78cfa7af628ce8707714d5
Rinse <rinse@vlaswinkel.info>
2021-05-18 11:12:18 +0200

    Attempt to fix .env test problem

--- test/gb/activity.test.ts
+++ test/gb/activity.test.ts
@@ -5,6 +5,13 @@ require('dotenv').config();
 
 jest.mock('axios');
 
+beforeAll(() => {
+    // Define constants
+    process.env.TOKEN_SECRET = 'test';
+    process.env.TOKEN_EXPIRES_IN = '30d';
+    process.env.TOKEN_ISSUER = 'https://tue.nl';
+});
+
 describe('with mocked activities get call', () => {
     // Test token we'll be using
     const mockToken = createJWT('0', 'testToken', 'refreshToken');

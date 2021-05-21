71381780e9915d33104a6514670da92b5516016d
20181880 <riklitjens@hotmail.nl>
2021-05-22 00:03:02 +0200

    Updated testing to include new standard + mapping meal types to default time

--- test/services/fooddiary.test.ts
+++ test/services/fooddiary.test.ts
@@ -1,5 +1,7 @@
 import { InsulinModel } from '../../src/gb/models/insulinModel';
+import { Food } from '../../src/gb/objects/food';
 import { OutputDataType } from '../../src/services/dataParsers/dataParser';
+import FoodDiaryParser, { FoodDiaryData } from '../../src/services/dataParsers/foodDiaryParser';
 import { parseFoodDiary } from './parseUtils';
 
 test('test robustness of Food diary data parser', async () => {
@@ -12,3 +14,179 @@ test('test robustness of Food diary data parser', async () => {
         )[1];
     }).rejects.toThrow('Wrong input data for processing food diary data!');
 });
+
+
+test('test automatic date + total insulin fill', async () => {
+    const rawFoodData: FoodDiaryData[] = [{
+        date: '08/05/21',
+        time: '13:12',
+        meal_type: '',
+        description: 'Pizza',
+        carbohydrates: '3',
+        glycemic_index: '2',
+        base_insulin: '4',
+        high_correction_insulin: '',
+        sports_correction_insulin: '',
+        total_insulin: '4'
+      },
+      {
+        date: '',
+        time: '00:00',
+        meal_type: 'Snack',
+        description: '',
+        carbohydrates: '5',
+        glycemic_index: '3',
+        base_insulin: '2',
+        high_correction_insulin: '1',
+        sports_correction_insulin: '',
+        total_insulin: ''
+      }]
+
+      const preprocessedFoodData = [{
+        date: '08/05/21',
+        time: '13:12',
+        meal_type: '',
+        description: 'Pizza',
+        carbohydrates: '3',
+        glycemic_index: '2',
+        base_insulin: '4',
+        high_correction_insulin: '',
+        sports_correction_insulin: '',
+        total_insulin: '4'
+      },
+      {
+        date: '08/05/21',
+        time: '00:00',
+        meal_type: 'Snack',
+        description: '',
+        carbohydrates: '5',
+        glycemic_index: '3',
+        base_insulin: '2',
+        high_correction_insulin: '1',
+        sports_correction_insulin: '',
+        total_insulin: '3'
+      }]
+      expect(
+        (
+            await FoodDiaryParser.preprocess(rawFoodData) 
+        )
+    ).toStrictEqual(preprocessedFoodData);
+})
+
+
+test('test automatic date + total insulin + time fill', async () => {
+    const rawFoodData: FoodDiaryData[] = [{
+        date: '08/05/21',
+        time: '13:12',
+        meal_type: '',
+        description: 'Pizza',
+        carbohydrates: '3',
+        glycemic_index: '2',
+        base_insulin: '4',
+        high_correction_insulin: '',
+        sports_correction_insulin: '',
+        total_insulin: '4'
+      },
+      {
+        date: '',
+        time: '',
+        meal_type: 'Breakfast',
+        description: '',
+        carbohydrates: '5',
+        glycemic_index: '3',
+        base_insulin: '2',
+        high_correction_insulin: '1',
+        sports_correction_insulin: '',
+        total_insulin: ''
+      },{
+        date: '08/05/21',
+        time: '08:00',
+        meal_type: '',
+        description: '',
+        carbohydrates: '5',
+        glycemic_index: '3',
+        base_insulin: '2',
+        high_correction_insulin: '1',
+        sports_correction_insulin: '',
+        total_insulin: '3'
+      }]
+
+      const preprocessedFoodData = [{
+        date: '08/05/21',
+        time: '13:12',
+        meal_type: '',
+        description: 'Pizza',
+        carbohydrates: '3',
+        glycemic_index: '2',
+        base_insulin: '4',
+        high_correction_insulin: '',
+        sports_correction_insulin: '',
+        total_insulin: '4'
+      },
+      {
+        date: '08/05/21',
+        time: '08:00',
+        meal_type: 'Breakfast',
+        description: '',
+        carbohydrates: '5',
+        glycemic_index: '3',
+        base_insulin: '2',
+        high_correction_insulin: '1',
+        sports_correction_insulin: '',
+        total_insulin: '3'
+      }, {
+        date: '08/05/21',
+        time: '08:00',
+        meal_type: '',
+        description: '',
+        carbohydrates: '5',
+        glycemic_index: '3',
+        base_insulin: '2',
+        high_correction_insulin: '1',
+        sports_correction_insulin: '',
+        total_insulin: '3'
+      }]
+
+      const mealTypeMap = new Map<string, string>();
+      mealTypeMap.set("Breakfast", "08:00");
+      expect(
+        (
+           await FoodDiaryParser.preprocess(rawFoodData, mealTypeMap) 
+        )
+    ).toStrictEqual(preprocessedFoodData);
+})
+
+
+test('test missing first date', async () => {
+    const rawFoodData: FoodDiaryData[] = [{
+        date: '',
+        time: '13:12',
+        meal_type: '',
+        description: 'Pizza',
+        carbohydrates: '3',
+        glycemic_index: '2',
+        base_insulin: '4',
+        high_correction_insulin: '',
+        sports_correction_insulin: '',
+        total_insulin: '4'
+      },
+      {
+        date: '',
+        time: '00:00',
+        meal_type: 'Snack',
+        description: '',
+        carbohydrates: '5',
+        glycemic_index: '3',
+        base_insulin: '2',
+        high_correction_insulin: '1',
+        sports_correction_insulin: '',
+        total_insulin: ''
+      }]
+
+      expect(
+        ( async () => {
+            await FoodDiaryParser.preprocess(rawFoodData) 
+        }
+        )
+    ).rejects.toThrow('First date needs to be filled in!');
+})

bd070af00326e80c213433a9735826b74fdb8e16
Rinse <rinse@vlaswinkel.info>
2021-05-24 11:46:23 +0200

    Added fake client credentials for .env tests

--- test/setupJest.ts
+++ test/setupJest.ts
@@ -3,3 +3,6 @@ require('jest-fetch-mock').enableMocks();
 process.env.TOKEN_SECRET = 'test';
 process.env.TOKEN_EXPIRES_IN = '30d';
 process.env.TOKEN_ISSUER = 'https://tue.nl';
+// These are just for testing purposes
+process.env.AZURE_CLIENT_SECRET = 'aaa';
+process.env.AZURE_CLIENT_ID = 'bbb';

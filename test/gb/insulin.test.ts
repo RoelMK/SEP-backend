7a4d6ee29d2c74ecee04dac43880dbff6de9b8b9
alexnik99 <alexnik99@abv.bg>
2021-06-03 22:02:46 +0200

    Add test cases for functions for retrieving insulin activities

--- test/gb/insulin.test.ts
+++ test/gb/insulin.test.ts
@@ -1,10 +1,11 @@
 import { TokenHandler } from '../../src/gb/auth/tokenHandler';
 import { GameBusClient } from '../../src/gb/gbClient';
+import { InsulinGameDescriptorNames } from '../../src/gb/objects/insulin';
 import { mockRequest } from '../testUtils/requestUtils';
 
 jest.mock('axios');
 
-describe('with mocked insulin get call', () => {
+describe('with mocked exercises get call', () => {
     // Request handler that simply returns empty data for every request
     const request = mockRequest(() => {
         return Promise.resolve({
@@ -16,24 +17,68 @@ describe('with mocked insulin get call', () => {
     beforeEach(() => request.mockClear());
 
     // GameBusClient using mockToken
-    const client = new GameBusClient(new TokenHandler('testToken', 'refreshToken', '0'));
+    const mockToken = 'testToken';
+    const client = new GameBusClient(new TokenHandler(mockToken, 'refreshToken', '0'));
 
-    test('GET activities on date', async () => {
-        // Get exercises from a date (as Date object)
-        // TODO: implement this
-        const insulin = await client.insulin().getAllInsulinActivities();
+    test('GET activities from type', async () => {
+        const insulin = await client
+            .insulin()
+            .getInsulinActivityFromGd(0, [InsulinGameDescriptorNames.LOG_INSULIN]);
 
         // Check that URL matches expected URL and mockToken is used in authorization
-        // expect(request).toHaveBeenCalledTimes(1);
-        // TODO: add URL
-        // expect(request).toHaveBeenCalledWith(
-        //     expect.objectContaining({
-        //         headers: expect.objectContaining({
-        //             Authorization: `Bearer ${mockToken}`
-        //         })
-        //     })
-        // );
-        //expect(exercises).toEqual([]);
-        expect(insulin).toEqual(undefined);
+        expect(request).toHaveBeenCalledTimes(1);
+        expect(request).toHaveBeenCalledWith(
+            expect.objectContaining({
+                headers: expect.objectContaining({
+                    Authorization: `Bearer ${mockToken}`
+                }),
+                url: 'https://api3.gamebus.eu/v2/players/0/activities?gds=LOG_INSULIN'
+            })
+        );
+        expect(insulin).toEqual([]);
+    });
+
+    test('GET activities from type between dates', async () => {
+        const unixTimestampBefore = new Date('2021-04-19').getTime();
+        const unixTimestampAfter = new Date('2021-04-21').getTime();
+        const insulin = await client
+            .insulin()
+            .getInsulinActivityFromGdBetweenUnix(
+                0,
+                [InsulinGameDescriptorNames.LOG_INSULIN],
+                unixTimestampBefore,
+                unixTimestampAfter
+            );
+        expect(request).toHaveBeenCalledTimes(1);
+        expect(request).toHaveBeenCalledWith(
+            expect.objectContaining({
+                headers: expect.objectContaining({
+                    Authorization: `Bearer ${mockToken}`
+                }),
+                url: 'https://api3.gamebus.eu/v2/players/0/activities?start=19-04-2021&end=21-04-2021&sort=-date&gds=LOG_INSULIN'
+            })
+        );
+        expect(insulin).toEqual([]);
+    });
+
+    test('GET activities from type on date', async () => {
+        const unixTimestamp = new Date('2021-04-19').getTime();
+        const insulin = await client
+            .insulin()
+            .getInsulinActivityFromGdOnUnixDate(
+                0,
+                [InsulinGameDescriptorNames.LOG_INSULIN],
+                unixTimestamp
+            );
+        expect(request).toHaveBeenCalledTimes(1);
+        expect(request).toHaveBeenCalledWith(
+            expect.objectContaining({
+                headers: expect.objectContaining({
+                    Authorization: `Bearer ${mockToken}`
+                }),
+                url: 'https://api3.gamebus.eu/v2/players/0/activities?start=19-04-2021&end=20-04-2021&sort=-date&gds=LOG_INSULIN'
+            })
+        );
+        expect(insulin).toEqual([]);
     });
 });

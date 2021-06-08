92a2d8fbf742af53367781ac28533188c0571822
Rinse <rinse@vlaswinkel.info>
2021-06-08 13:31:11 +0200

    Surpressed "unused" error since it's clearly being used

--- src/gb/gbClient.ts
+++ src/gb/gbClient.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import axios, { AxiosError, AxiosInstance } from 'axios';
 import { TokenHandler } from './auth/tokenHandler';
 import { Activity } from './objects/activity';

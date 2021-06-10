346c72e21b656edead50761351d889c19cf43746
alexnik99 <alexnik99@abv.bg>
2021-06-10 23:28:45 +0200

    Comments and lint fixes SUPERVISOR

--- src/utils/supervisorUtils.ts
+++ src/utils/supervisorUtils.ts
@@ -1,58 +1,83 @@
-import jwt from 'jsonwebtoken';
 import { DBClient } from '../db/dbClient';
-import { GameBusClient } from '../gb/gbClient';
-import crypto from 'crypto';
 
 /**
- * Finishes a login attempt and generates a JWT with credentials.
- * @param loginToken Login token of attempt to finish
- * @returns A JWT with credentials or undefined if failed to finish login attempt
+ * Log token for a user
+ * @param email Email of the user
+ * @param loginToken Login token of the user
+ * @returns If entry in the tokens table is added successfully
  */
-export function logToken(email: string, loginToken: string ): Boolean {
-  const dbClient: DBClient = new DBClient();
+export function logToken(email: string, loginToken: string): boolean {
+    const dbClient: DBClient = new DBClient();
 
-  const registered = dbClient.logToken(email, loginToken);
-  dbClient.close();
-
-  return registered;
+    const registered = dbClient.logToken(email, loginToken);
+    dbClient.close();
+    return registered;
 }
 
+/**
+ * Request supervisor role from a supervisor
+ * @param supervisorEmail Email of the supervisor user
+ * @param childEmail Email of the normal user
+ * @param confirmed Boolean which is true if the request is confirmed,
+ * false otherwise
+ * @returns If request/confirmation is successful
+ */
 export function request(supervisorEmail: string, childEmail: string, confirmed?: boolean): boolean {
-  const dbClient: DBClient = new DBClient();
+    const dbClient: DBClient = new DBClient();
 
-  let success = false;
-  if (confirmed) {
-    success = dbClient.confirmSupervisor(supervisorEmail, childEmail);
-  } else {
-    success = dbClient.requestSupervisor(supervisorEmail, childEmail);
-  }
-  dbClient.close();
-
-  return success;
+    let success = false;
+    if (confirmed) {
+        success = dbClient.confirmSupervisor(supervisorEmail, childEmail);
+    } else {
+        success = dbClient.requestSupervisor(supervisorEmail, childEmail);
+    }
+    dbClient.close();
+    return success;
 }
 
-export function getTokens(supervisorEmail): any {
-  const dbClient: DBClient = new DBClient();
-  const tokens = dbClient.getChildTokens(supervisorEmail);
-  return tokens;
+/**
+ * Get tokens of children for a supervisor
+ * @param supervisorEmail Email of the supervisor user
+ * @returns Tokens of children for a supervisor user
+ */
+export function getTokens(supervisorEmail: string): any {
+    const dbClient: DBClient = new DBClient();
+    const tokens = dbClient.getChildTokens(supervisorEmail);
+    return tokens;
 }
 
-export function getSupervisors(childEmail): any {
-  const dbClient: DBClient = new DBClient();
-  const tokens = dbClient.getRequestedSupervisors(childEmail);
-  return tokens;
+/**
+ * Get list of requested supervisor users for a normal user
+ * @param childEmail Email of the normal user
+ * @returns List of requested supervisor users
+ */
+export function getSupervisors(childEmail: string): any {
+    const dbClient: DBClient = new DBClient();
+    const tokens = dbClient.getRequestedSupervisors(childEmail);
+    return tokens;
 }
 
-export function getChildren(supervisorEmail): any {
-  const dbClient: DBClient = new DBClient();
-  const tokens = dbClient.getRequestedChildren(supervisorEmail);
-  return tokens;
+/**
+ * Get list of normal users who have requested supervisor
+ * role from supervisor users
+ * @param supervisorEmail Email of the supervisor user
+ * @returns List of normal users requested supervisor role
+ */
+export function getChildren(supervisorEmail: string): any {
+    const dbClient: DBClient = new DBClient();
+    const tokens = dbClient.getRequestedChildren(supervisorEmail);
+    return tokens;
 }
 
-export function retractPermission(childEmail, supervisorEmail) {
-  const dbClient: DBClient = new DBClient();
-  const success = dbClient.retractPermission(childEmail, supervisorEmail);
-
-  return success;
-
+/**
+ * Retract permission for a supervisor user to supervise
+ * normal user
+ * @param supervisorEmail Email of the supervisor user
+ * @param childEmail Email of the normal user
+ * @returns If the retractation is successful
+ */
+export function retractPermission(childEmail: string, supervisorEmail: string): boolean {
+    const dbClient: DBClient = new DBClient();
+    const success = dbClient.retractPermission(childEmail, supervisorEmail);
+    return success;
 }

166a2eb0dadc622c2bbf41c03241c3e2da1b8753
Rinse <rinse@vlaswinkel.info>
2021-06-12 17:05:14 +0200

    Added POST besides PUT for insulin

--- src/routes/insulin.ts
+++ src/routes/insulin.ts
@@ -9,35 +9,61 @@ import { validUnixTimestamp } from '../services/utils/dates';
 const insulinRouter = Router();
 
 insulinRouter.post('/insulin', checkJwt, async (req: any, res: Response) => {
-    // Simple POST should only include an InsulinModel
+    // For now, the 'modify' parameter will be used to distinguish between POST and PUT
+    // modify as boolean (only important if true)
 
     const insulinTime = req.body.timestamp as number;
     const insulinAmount = req.body.insulinAmount as number;
     const insulinType = req.body.insulinType as InsulinType;
-    const activityId = req.body.activityId as number;
 
     // Verify that we have a valid amount, an activityId, an insulinType and a valid timestamp
-    if (!validUnixTimestamp(insulinTime) || insulinAmount < 0 || !activityId || !insulinType) {
+    if (!validUnixTimestamp(insulinTime) || insulinAmount < 0 || !insulinType) {
         // Bad request
         return res.sendStatus(400);
     }
 
-    const insulinModel: InsulinModel = {
+    let insulinModel: InsulinModel = {
         timestamp: insulinTime,
         insulinAmount: insulinAmount,
-        insulinType: insulinType,
-        activityId: activityId
+        insulinType: insulinType
     };
 
     const gbClient = new GameBusClient(
         new TokenHandler(req.user.accessToken, req.user.refreshToken, req.user.playerId)
     );
 
+    // PUT
+    if (req.query.modify) {
+        if (!req.body.activityId) {
+            // Bad request, missing activity ID for PUT
+            res.sendStatus(400);
+        }
+        const activityId = req.body.activityId as number;
+        insulinModel = {
+            ...insulinModel,
+            activityId: activityId
+        };
+        try {
+            // PUT data
+            await gbClient.insulin().putSingleInsulinActivity(insulinModel, req.user.playerId);
+            // Send 200 and new model
+            return res.status(201).send(insulinModel);
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                // Unauthorized -> 401
+                if (error.response?.status === 401) {
+                    return res.status(401).send();
+                }
+            }
+            // Unknown error -> 503
+            return res.status(503).send();
+        }
+    }
+
+    // POST
     try {
-        // PUT data
-        await gbClient.insulin().putSingleInsulinActivity(insulinModel, req.user.playerId);
-        // Send 200 and new model
-        return res.status(200).send(insulinModel);
+        gbClient.insulin().postSingleInsulinActivity(insulinModel, req.user.playerId);
+        res.sendStatus(201).send(insulinModel);
     } catch (error) {
         if (axios.isAxiosError(error)) {
             // Unauthorized -> 401

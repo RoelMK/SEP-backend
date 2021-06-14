b2322e0544357758163f17dd1c9f7cc2d663d1b0
20181880 <riklitjens@hotmail.nl>
2021-06-14 11:48:56 +0200

    Added GameBus functions

--- src/gb/models/BMIModel.ts
+++
@@ -1,9 +0,0 @@
-export interface BMIModel {
-    timestamp: number;
-    weight: number | null; // weight in kg, should not be optional since we POST it
-    length: number | null; // height in cm
-    age: number | null; // age in years
-    gender?: string | null; // Either m, f or o, optional since we don't really need it
-    waistCircumference?: number | null; // in cm, optional since we don't really need it
-    bmi?: number | null; // in kg/m^2, optional since we don't really need it
-}
--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,36 +1,51 @@
+import axios from 'axios';
 import { Router } from 'express';
-import { GameBusToken } from '../gb/auth/tokenHandler';
-import { BMIModel } from '../gb/models/BMIModel';
+import { TokenHandler } from '../gb/auth/tokenHandler';
+import { GameBusClient } from '../gb/gbClient';
+import { BMIModel } from '../gb/models/bmiModels';
 import { checkJwt } from '../middlewares/checkJwt';
 
 const profileRouter = Router();
 
-profileRouter.get('/profile', checkJwt, (req: any, res: any) => {
-    // retrieve user information
-    const userInfo: GameBusToken = {
-        playerId: req.user.playerId,
-        accessToken: req.user.accessToken,
-        refreshToken: req.user.refreshToken
-    };
+/**
+ * Endpoint for retrieving profile data of the user
+ */
+profileRouter.get('/profile', checkJwt, async (req: any, res: any) => {
+    // use user info to create a GameBus client
+    const gbClient: GameBusClient = new GameBusClient(
+        new TokenHandler(req.user.accessToken, req.user.refreshToken, req.user.playerId)
+    );
 
-    //TODO
-
-    res.status(200).send('Successfully retrieved profile information');
+    try {
+        const profileData: BMIModel = await gbClient.bmi().getLatestBMIActivity(req.user.playerId);
+        return res.status(200).json(profileData).send('Successfully retrieved profile information');
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            if (error.response?.status === 401) {
+                // Unauthorized
+                return res.status(401).send();
+            }
+        }
+        // We cannot handle this specific request, no data for the user.
+        return res.status(503).send();
+    }
 });
 
-profileRouter.post('/profile', checkJwt, (req: any, res: any) => {
+/**
+ * Endpoint for posting profile data to gamebus
+ */
+profileRouter.post('/profile', checkJwt, async (req: any, res: any) => {
     if (!req.query.weight || !req.query.length || !req.query.age) {
         res.status(400).send();
         return;
     }
 
-    // retrieve user information
-    const userInfo: GameBusToken = {
-        playerId: req.user.playerId,
-        accessToken: req.user.accessToken,
-        refreshToken: req.user.refreshToken
-    };
+    // use user info to create a GameBus client
+    const gbClient: GameBusClient = new GameBusClient(
+        new TokenHandler(req.user.accessToken, req.user.refreshToken, req.user.playerId)
+    );
 
+    // extract parameters into bmi model
     const bmi: BMIModel = {
         // necessary properties
         timestamp: new Date().getTime(),
@@ -43,8 +58,20 @@ profileRouter.post('/profile', checkJwt, (req: any, res: any) => {
         ...(req.query.bmi && { bmi: req.query.bmi })
     };
 
-    // TODO
-    res.status(200).send('Successfully posted profile information');
+    try {
+        // tries posting the bmi profile data to GameBus
+        await gbClient.bmi().postSingleBMIActivity(bmi, req.user.playerId);
+        return res.status(200).send('Successfully posted profile information');
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            if (error.response?.status === 401) {
+                // Unauthorized
+                return res.status(401).send();
+            }
+        }
+        // We cannot handle this specific request, no data for the user.
+        return res.status(503).send();
+    }
 });
 
 module.exports = profileRouter;

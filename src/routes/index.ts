7d421ee9ea46ad14bb933139bede653185b0b485
Roel Koopman <r.m.koopman@student.tue.nl>
2021-06-18 11:41:43 +0200

    Added cert verification url

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -13,5 +13,8 @@ indexRouter.use('/', require('./mood'));
 indexRouter.use('/', require('./insulin'));
 indexRouter.use('/', require('./activity'));
 indexRouter.use('/', require('./profile'));
+indexRouter.use('/.well-known/acme-challenge/lX3DEZfFchxrG_xfG_2rPOUefvmYLmxYw9EJlQtVQ1k', (req: any, res: any) => {
+    res.status(200).send('lX3DEZfFchxrG_xfG_2rPOUefvmYLmxYw9EJlQtVQ1k.mQvT2UanQpUOo6C2OCOVh17S8l2ZOCtz7X2cJtIM7fo');
+});
 
 module.exports = indexRouter;

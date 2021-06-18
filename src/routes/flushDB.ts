79ea7d77bb378862c5ab9858957c3fc12926c4a2
20181880 <riklitjens@hotmail.nl>
2021-06-18 13:05:54 +0200

    eslint

--- src/routes/flushDB.ts
+++ src/routes/flushDB.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { DBClient } from '../db/dbClient';
+/* eslint-disable @typescript-eslint/no-var-requires */
 require('dotenv').config();
 
 const router = Router();
